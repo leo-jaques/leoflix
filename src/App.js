08abde641c1bb43fc9152dfc7630022037a1c911
leo-jaques <leo.jaques67@gmail.com>
2022-06-29 14:30:20 -0300

    loaded data in main app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
+
   return (
-    <div>
+    <div className='page'>
       Hello World
     </div>
   );
